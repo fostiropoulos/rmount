09cde64d7bc3b746619e571e3721d1522f755f4b
Iordanis Fostiropoulos <fostiropoulos@users.noreply.github.com>
2023-09-12 16:39:57 +0000

    updated tests to work with docker volumes and inside a container

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,59 +1,69 @@
 import copy
 import os
-from pathlib import Path
 import time
+from pathlib import Path
+
 import docker
-from docker.errors import NotFound
 import paramiko
 import pytest
-from rmount.config import Remote
+from docker.errors import NotFound
 
+from rmount.config import Remote
 from rmount.main import RemoteMount
 
 SSH_USER = "admin"
 SSH_PORT = 2222
+# Used to make sure the permissions between docker and local
+# are correct.
+USER_UID = os.getuid()
 DOCKER_CONFIG = {
     "PUBLIC_KEY": None,
     "SUDO_ACCESS": False,
     "PASSWORD_ACCESS": False,
     "USER_NAME": SSH_USER,
-    "PUID": 1000,
-    "PGID": 1000,
+    "PUID": USER_UID,
+    "PGID": USER_UID,
 }
 DOCKER_CONTAINER_NAME = "rmount-test-server"
 
 
-def docker_kill_running_containers(docker_client: docker.DockerClient):
-    for c in docker_client.api.containers(filters={"name": DOCKER_CONTAINER_NAME}):
+def docker_kill_running_containers(
+    docker_client: docker.DockerClient, container_name: str
+):
+    for c in docker_client.api.containers(filters={"name": container_name}):
         docker_client.api.kill(c)
     for i in range(10):
         try:
-            docker_client.api.remove_container(DOCKER_CONTAINER_NAME)
+            docker_client.api.remove_container(container_name)
         except NotFound:
             return
         except Exception:
             continue
         time.sleep(1)
 
-    raise RuntimeError(f"Could not remove {DOCKER_CONTAINER_NAME}")
+    raise RuntimeError(f"Could not remove {container_name}")
 
 
 def docker_run_cmd(
-    docker_client: docker.DockerClient, public_key: str, volume_path: Path
+    docker_client: docker.DockerClient,
+    public_key: str,
+    volume_remote_path: Path,
+    volume_mountpoint: str | Path,
+    container_name: str,
 ):
     environment = copy.deepcopy(DOCKER_CONFIG)
     environment["PUBLIC_KEY"] = public_key
-    docker_kill_running_containers(docker_client)
-    volume_path.mkdir(exist_ok=True, parents=True)
-    vp = volume_path.as_posix()
+    docker_kill_running_containers(docker_client, container_name)
+    volume_remote_path.mkdir(exist_ok=True, parents=True)
+    vp = volume_remote_path.as_posix()
 
     img = docker_client.containers.run(
-        name=DOCKER_CONTAINER_NAME,
+        name=container_name,
         image="lscr.io/linuxserver/openssh-server:latest",
         environment=environment,
         ports={SSH_PORT: SSH_PORT},
         detach=True,
-        volumes={vp: {"bind": vp, "mode": "rw"}},
+        volumes={volume_mountpoint: {"bind": vp, "mode": "rw"}},
         remove=True,
     )
     return img
@@ -79,9 +89,9 @@ def _public_key(tmp_path: Path):
     return public_key
 
 
-def _config(tmp_path: Path):
+def _config(tmp_path: Path, host: str = "localhost"):
     config = {
-        "host": "localhost",
+        "host": host,
         "user": SSH_USER,
         "port": SSH_PORT,
         "key_file": tmp_path.joinpath("id_rsa"),
@@ -92,45 +102,75 @@ def _config(tmp_path: Path):
 
 def _rmount(tmp_path: Path, config) -> RemoteMount:
     remote_path = tmp_path / "remote_path"
-    remote_path.mkdir(exist_ok=True)
     mount_path = tmp_path / "mount_path"
     rmount = RemoteMount(
         config,
         remote_path=remote_path,
         local_path=mount_path,
         refresh_interval_s=1,
-        timeout=10,
+        timeout=20,
+        verbose=True,
     )
     return rmount
 
 
-def _remote_server(tmp_path: Path) -> "RemoteServer":
+def _remote_server(
+    tmp_path: Path, volume_mountpoint: Path | str | None = None
+) -> "RemoteServer":
     pub_key = _public_key(tmp_path)
-    return RemoteServer(pub_key, tmp_path.joinpath("remote_path"))
+
+    if volume_mountpoint is not None:
+        remote_path = Path("/tmp")
+    else:
+        remote_path = tmp_path.joinpath("remote_path")
+    if volume_mountpoint is None:
+        volume_mountpoint = remote_path
+    return RemoteServer(pub_key, remote_path, volume_mountpoint=volume_mountpoint)
+
+
+def pytest_addoption(parser):
+    parser.addoption(
+        "--volume-mountpoint",
+        action="store",
+        default=None,
+        help=(
+            "the volume name to optionally use. "
+            "Used only for docker-to-docker tests with shared volumes"
+        ),
+    )
 
 
 @pytest.fixture(autouse=True)
-def remote_server(tmp_path: Path):
-    with _remote_server(tmp_path) as s:
+def remote_server(tmp_path: Path, pytestconfig):
+    volume_mountpoint = pytestconfig.getoption("--volume-mountpoint")
+    with _remote_server(tmp_path, volume_mountpoint) as s:
         yield s
 
 
 @pytest.fixture
 def rmount(tmp_path, config):
-    with _rmount(tmp_path, config) as r:
-        yield r
+    r = _rmount(tmp_path, config)
+    r.mount()
+    yield r
+    r.unmount()
+    time.sleep(3)
 
 
 @pytest.fixture
-def config(tmp_path):
-    return _config(tmp_path)
+def config(tmp_path, remote_server: "RemoteServer"):
+    return _config(tmp_path, remote_server.ip_address)
 
 
 class RemoteServer:
-    def __init__(self, public_key: str, volume_path: Path | str) -> None:
+    def __init__(
+        self, public_key: str, volume_path: Path | str, volume_mountpoint: Path | str
+    ) -> None:
         self.public_key = public_key
         self.volume_path = Path(volume_path)
+        self.volume_mountpoint = volume_mountpoint
+        self.ip_address = None
         self.img = None
+        self.container_name = DOCKER_CONTAINER_NAME
         try:
             self.client = docker.from_env()
         except Exception as e:
@@ -155,7 +195,16 @@ class RemoteServer:
         return self
 
     def kill(self):
-        docker_kill_running_containers(self.client)
+        docker_kill_running_containers(self.client, self.container_name)
 
     def start(self):
-        self.img = docker_run_cmd(self.client, self.public_key, self.volume_path)
+        self.img = docker_run_cmd(
+            self.client,
+            self.public_key,
+            self.volume_path,
+            self.volume_mountpoint,
+            container_name=self.container_name,
+        )
+        self.ip_address = self.client.containers.get(self.img.id).attrs[
+            "NetworkSettings"
+        ]["IPAddress"]
--- tests/test_main.py
+++ tests/test_main.py
@@ -21,14 +21,13 @@ from pathlib import Path
 import pytest
 
 from rmount import RemoteMount, main
-from rmount.utils import unmount
+from rmount.utils import terminate, unmount
 
 main.HEARTBEAT_ERROR_COUNT = 2
-main.MISSED_HEARTBEATS_ERROR = 2
-
+main.RESTART_ERROR = 2
 
 logger = logging.getLogger("RMount")
-logger.setLevel(logging.INFO)
+logger.setLevel(logging.WARN)
 
 if os.name != "posix":
     raise NotImplementedError("Unsupported OS.")
@@ -42,7 +41,7 @@ def write_bytes(tmp_file: Path):
 
 
 def _assert_with_timeout(fn):
-    for i in range(10):
+    for i in range(30):
         try:
             assert fn()
             return True
@@ -53,6 +52,7 @@ def _assert_with_timeout(fn):
 
 
 def test_mount_remount(rmount: RemoteMount, remote_server: "RemoteServer"):
+    """Tests whether the storage is persistent between mounts / unmounts."""
     local_path = rmount.local_path
     remote_path = rmount.remote_path
     lfile = local_path.joinpath("local.txt")
@@ -87,122 +87,166 @@ def test_mount_remount(rmount: RemoteMount, remote_server: "RemoteServer"):
 
 
 def test_reconnection(rmount: RemoteMount, remote_server: "RemoteServer"):
+    """Tests what happens when the connection between rmount and a remote
+    suddenly drops but is then restored."""
     assert rmount.is_alive()
     remote_server.kill()
     assert not rmount.is_alive(timeout=5)
     remote_server.start()
-    for i in range(10):
+    for i in range(30):
         if rmount.is_alive(timeout=5):
-            break
+            return
         time.sleep(1)
-    assert rmount.is_alive()
+    assert False
 
 
 def test_connection_drop(rmount: RemoteMount, remote_server: "RemoteServer"):
+    """Tests what happens when the connection between rmount and a remote
+    suddenly drops."""
     rmount.unmount()
     remote_server.kill()
-    is_alive = multiprocessing.Event()
-    is_alive.clear()
+    is_dead = multiprocessing.Event()
+    is_dead.clear()
 
-    def _run_error(is_alive):
+    def _error_callback(is_dead):
+        is_dead.set()
+
+    rmount._error_callback = lambda: _error_callback(is_dead)
+
+    def _run_error():
         remote_server.start()
         rmount.mount()
         assert rmount.is_alive()
-        remote_server.kill()
-        assert not rmount.is_alive(timeout=5)
-        for i in range(120):
-            time.sleep(1)
-        is_alive.set()
-        return
 
-    p = multiprocessing.Process(target=_run_error, args=(is_alive,))
+        remote_server.kill()
+
+        if rmount.is_alive(timeout=5):
+            assert False, "rmount did not die on connection drop"
+
+    p = multiprocessing.Process(
+        target=_run_error,
+    )
     p.start()
-    while p.is_alive() and not is_alive.is_set():
-        p.join(1)
-    assert not is_alive.is_set()
+    for i in range(120):
+        if is_dead.is_set():
+            break
+        time.sleep(1)
+    assert is_dead.is_set()
 
 
 def test_no_remote(rmount: RemoteMount, remote_server: "RemoteServer"):
+    """Tests what happens when trying to connect to an invalid remote."""
     rmount.unmount()
     remote_server.kill()
-    with pytest.raises(AssertionError, match="Could not mount on time."):
+    with pytest.raises(RuntimeError, match="Could not mount on time."):
         rmount.mount()
 
 
 def test_context(rmount: RemoteMount, remote_server: "RemoteServer"):
+    """Tests whether the context manager works as expected."""
+    rmount.unmount()
     with rmount:
         assert rmount.is_alive()
     assert not rmount.is_alive()
 
 
 def test_interupt_upload(rmount: RemoteMount, remote_server: "RemoteServer"):
-    def delayed_unmount():
-        time.sleep(10)
-        rmount.unmount()
+    write_lock = multiprocessing.Lock()
 
-    slow_kill = multiprocessing.Process(target=delayed_unmount)
+    n_files = 3
+
+    def delayed_unmount(queue: multiprocessing.Queue, write_lock):
+        time.sleep(5)
+        write_lock.acquire(block=True)
+        queue.put(
+            [
+                rmount.local_path.joinpath(f"{i:02d}").read_bytes()
+                for i in range(n_files)
+            ]
+        )
+        terminate()
+        rmount._is_alive.clear()
+        rmount._is_alive.wait(timeout=5)
+        rmount._heart.kill()
+        rmount._is_alive.set()
+        unmount(rmount.local_path, timeout=rmount._timeout)
+
+    q = multiprocessing.Queue()
+    slow_kill = multiprocessing.Process(target=delayed_unmount, args=(q, write_lock))
     slow_kill.start()
-    n_files = 10
     n_numbers = 10_000_000
     # should be around 100MB of files
-    while slow_kill.is_alive():
-        name = f"{random.randint(0,n_files):02d}"
-        rmount.local_path.joinpath(name).write_bytes(
-            bytes(random.randint(n_numbers - 1, n_numbers * 10))
-        )
-    ints_1 = [
-        rmount.local_path.joinpath(f"{i:02d}").read_bytes() for i in range(n_files)
+    while True:
+        try:
+            if not write_lock.acquire(block=True, timeout=1):
+                break
+            name = f"{random.randint(0,n_files):02d}"
+            data = bytes(random.randint(n_numbers - 1, n_numbers * 10))
+            rmount.local_path.joinpath(name).write_bytes(data)
+            write_lock.release()
+            time.sleep(0.1)
+        except:
+            pass
+    ints_1 = q.get()
+    ints_2 = [
+        rmount.remote_path.joinpath(f"{i:02d}").read_bytes() for i in range(n_files)
     ]
 
-    def files_equal():
-        time.sleep(2)
-        ints_2 = [
-            rmount.remote_path.joinpath(f"{i:02d}").read_bytes() for i in range(n_files)
-        ]
-        return all(ints_1[i] == ints_2[i] for i in range(10)) and all(
-            ints_2[0] != ints_2[i] for i in range(1, 10)
-        )
-
-    _assert_with_timeout(files_equal)
+    assert all(ints_1[i] == ints_2[i] for i in range(n_files)) and all(
+        ints_2[0] != ints_2[i] for i in range(1, n_files)
+    )
 
 
 if __name__ == "__main__":
     from tests.conftest import RemoteServer, _config, _remote_server, _rmount
 
+    logger.setLevel(logging.DEBUG)
     """
-    # NOTE to test the connection to the mock server use:
+    NOTE Because the tests need to recover from an error to run again, it can
+    be the case that the tests fail when run one after the other but pass when run
+    independently. As long as they pass when run independently it should be sufficient.
+
+
+    NOTE to test the connection to the mock server use:
+
     $ ssh -p 2222 -i /tmp/rmount/test/id_rsa -o StrictHostKeyChecking=no \
           admin@localhost
 
-    # NOTE To test whether you can mount e.g. there are no RemoteServer errors:
+    NOTE To test whether you can mount e.g. there are no RemoteServer errors:
+
     $ sudo apt-get install sshfs
     $ sshfs -o default_permissions -o ssh_command='ssh -p 2222 -i \
         /tmp/rmount/test/id_rsa -o StrictHostKeyChecking=no' \
         -o cache_timeout=30 admin@localhost:/tmp/rmount/test/remote_path \
         /tmp/rmount/test/mount_path
 
-    # checks if it is still mounted
+    checks if it is still mounted
+
     $ mountpoint mount_path
     $ unmount fusermount -uz mount_path
     """
     test_fns = [
+        # test_mount_remount,
+        # test_reconnection,
+        test_connection_drop,
         test_interupt_upload,
         test_context,
-        test_mount_remount,
-        test_connection_drop,
         test_no_remote,
-        test_reconnection,
     ]
-    tmp_path = Path("/tmp/rmount/test")
-    unmount(tmp_path.joinpath("mount_path"), timeout=5)
-    shutil.rmtree(tmp_path, ignore_errors=True)
-    tmp_path.mkdir(parents=True, exist_ok=True)
-    config = _config(tmp_path)
-    s = _remote_server(tmp_path=tmp_path)
-    r = _rmount(tmp_path=tmp_path, config=config)
+
     for test_fn in test_fns:
+        tmp_path = Path("/tmp/rmount/test")
+        unmount(tmp_path.joinpath("mount_path"), timeout=5)
+        shutil.rmtree(tmp_path, ignore_errors=True)
+        tmp_path.mkdir(parents=True, exist_ok=True)
+        s = _remote_server(tmp_path=tmp_path)
         s.start()
+        config = _config(tmp_path, s.ip_address)
+        r = _rmount(tmp_path=tmp_path, config=config)
         r.mount()
+        logger.info(f"Testing {test_fn}.")
         test_fn(r, s)
+        logger.info("Passed.")
         r.unmount()
         s.kill()
+        time.sleep(1)
