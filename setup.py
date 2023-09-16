e2661ba5685747d925b0a3fbe9c7f8a6f2ff1731
Iordanis Fostiropoulos <fostiropoulos@users.noreply.github.com>
2023-09-16 17:16:23 +0000

    update version

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ else:
 
 setuptools.setup(
     name="rmount",
-    version="0.0.1",
+    version="0.0.2",
     author="Iordanis Fostiropoulos",
     author_email="mail@iordanis.me",
     description="A robust file-system mount to a remote storage.",
